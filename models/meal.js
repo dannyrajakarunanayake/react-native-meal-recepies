478f40f6ee6354552b4090879c272d0e43669653
Danny Ferdinandusz <dannyferdinandusz@dannys-iMac.local>
2021-09-10 13:02:59 +1000

    initial committ

---
+++ models/meal.js
@@ -0,0 +1,32 @@
+class Meal {
+  constructor(
+    id,
+    title,
+    categoryIds,
+    affordability,
+    complexity,
+    imageUrl,
+    duration,
+    ingredients,
+    steps,
+    isGlutenFree,
+    isVegan,
+    isVegetarian,
+    isLactoseFree
+  ) {
+    this.id = id;
+    this.title = title;
+    this.categoryIds = categoryIds;
+    this.affordability = affordability;
+    this.complexity = complexity;
+    this.imageUrl = imageUrl;
+    this.duration = duration;
+    this.ingredients = ingredients;
+    this.steps = steps;
+    this.Vegan = isVegan;
+    this.isVegetarian = isVegetarian;
+    this.isLactoseFree = isLactoseFree;
+  }
+}
+
+export default Meal;
